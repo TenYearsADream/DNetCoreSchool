044e9cd78bd0525ec4d4d56628d85551f5f57151
1176277624@qq.com <1176277624@qq.com>
2018-07-30 22:59:22 +0800

    efcoreMysql数据库控制台项目

---
+++ efcoreConsole2/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace efcoreConsole2
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //("Server=127.0.0.1;database=zszdb;uid=root;pwd=root");
+            var etPerson = modelBuilder.Entity<Person>();
+            etPerson.ToTable("t_persons");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql("Server=192.168.1.104;database=efcoreTest;uid=root;pwd=123456");
+        }
+    }
+}
---
+++ efcoreConsole2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace efcoreConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (MyDbContext dbContext = new MyDbContext())
+            {
+                Person per = new Person();
+                per.Name = "王基龙";
+                per.Age = 12;
+                per.Gender = true;
+                dbContext.Persons.Add(per);
+                dbContext.SaveChanges();
+            }
+            Console.WriteLine("ok");
+            Console.ReadLine();
+        }
+    }
+}
